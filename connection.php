3288878cd8757e615d8ac80ad8a0b9c22713f8ac
kxtong001 <86936351+kxtong001@users.noreply.github.com>
2021-09-07 06:10:59 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $cleardb_url = parse_url(getenv("mysql://b13d2a62a9a1fd:8f70ec7c@us-cdbr-east-04
 $cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.net"];
 $cleardb_username = $cleardb_url["b13d2a62a9a1fd"];
 $cleardb_password = $cleardb_url["8f70ec7c"];
-$cleardb_db = substr($cleardb_url["tuitionapp"],1);
+$cleardb_db = substr($cleardb_url["heroku_179fff40fae295c"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
