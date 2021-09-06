f67dc40be6bbd20193bc54c90b9d813d8b2a6fd5
kxtong001 <86936351+kxtong001@users.noreply.github.com>
2021-09-07 06:07:37 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("mysql://b13d2a62a9a1fd:8f70ec7c@us-cdbr-east-04.cleardb.com/heroku_179fff40fae295c?reconnect=true"));
 $cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.net"];
 $cleardb_username = $cleardb_url["b13d2a62a9a1fd"];
 $cleardb_password = $cleardb_url["8f70ec7c"];
