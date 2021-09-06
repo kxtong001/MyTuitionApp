e334e89ac21cf200fe15a9352eaa55a99fe00c79
kxtong001 <86936351+kxtong001@users.noreply.github.com>
2021-09-07 06:04:27 +0800

    Update connection.php
    connect to heroku

--- connection.php
+++ connection.php
@@ -1,6 +1,12 @@
 <?php
-
-	$db=mysqli_connect("localhost","root","","tuitionapp");  
-					// server name, username, password, database name
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.net"];
+$cleardb_username = $cleardb_url["b13d2a62a9a1fd"];
+$cleardb_password = $cleardb_url["8f70ec7c"];
+$cleardb_db = substr($cleardb_url["tuitionapp"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
