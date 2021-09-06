3a246b2cf93c5e0e4860f3c454d172b70e88fdbb
kxtong001 <86936351+kxtong001@users.noreply.github.com>
2021-09-07 06:22:42 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,10 @@
 <?php
+$url = parse_url(getenv("mysql:\b13d2a62a9a1fd:8f70ec7c@us-cdbr-east-04.cleardb.com\heroku_179fff40fae295c?reconnect=true"));
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("mysql://b13d2a62a9a1fd:8f70ec7c@us-cdbr-east-04.cleardb.com/heroku_179fff40fae295c?reconnect=true"));
-$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.net"];
-$cleardb_username = $cleardb_url["b13d2a62a9a1fd"];
-$cleardb_password = $cleardb_url["8f70ec7c"];
-$cleardb_db = substr($cleardb_url["heroku_179fff40fae295c"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+$server = $url["us-cdbr-east-04.cleardb.com"];
+$username = $url["b13d2a62a9a1fd"];
+$password = $url["8f70ec7c"];
+$database = substr($url["heroku_179fff40fae295c"], 1);
 // Connect to DB
-$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$db = mysqli_connect($server, $username, $password, $db);
 ?>
